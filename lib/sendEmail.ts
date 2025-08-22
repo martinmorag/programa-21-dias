4ef652df104756f2c2d39747e6749d16da5dacb0
Martin Moraga <moragamartin2@gmail.com>
2025-08-22 12:12:56 -0300

    updated nodemailer and process.env

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,10 +1,10 @@
-import nodemailer from 'nodemailer';
+import * as nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: "live.smtp.mailtrap.io",
-  port: 587,
+  host: process.env.EMAIL_SERVER_HOST,
+  port: parseInt(process.env.EMAIL_SERVER_PORT || '', 10),
   auth: {
-    user: "api",
+    user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD
   }
 });
